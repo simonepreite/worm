1bf363f109944d7793a069b69b8a40334d68a3b8
simone preite <simone-preite_89@hotmail.it>
2016-04-12 17:08:20 +0200

    up

--- player.hpp
+++ player.hpp
@@ -1,21 +1,24 @@
 #ifndef PLAYER_HPP_
 #define PLAYER_HPP_
 
+#define MAXNOME 8
+
 class player {
 protected:
 	/* informazioni utente */
-	string nome;
+	char nome[MAXNOME];
 	int id;
 	int x;
 	int y;	
 
 public:
-	player(){
+	player(int id_user, char player[]){
 		x = 0;
 		y = 0;
-		id = 0;
-		nome = "\0";
+		id = id_user;
+		strcpy(nome, player);
 	}
+	
 
 };
 #endif
