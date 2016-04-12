32c2b238e8c32ee54b9488e4073a08202a441130
simone preite <simone-preite_89@hotmail.it>
2016-04-12 16:35:35 +0200

    up

---
+++ player.hpp
@@ -0,0 +1,16 @@
+#ifndef PLAYER_HPP_
+#define PLAYER_HPP_
+class player {
+protected:
+	/* coordinate della mappa */
+	string nome;
+	int x;
+	int y;
+	/* puntatori alle direzioni */
+	
+
+public:
+
+
+};
+#endif
---
+++ worms.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <iostream>
+#include <node.hpp>
+#include <player.hpp>
+
+using namespace std;
