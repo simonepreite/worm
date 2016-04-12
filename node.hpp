17fe57e7fafbd12a1ea4ebb9f3ea617a818534e1
= <gabriorazi07@gmail.com>
2016-04-12 17:13:20 +0200

    prima versione della classe nodo

--- node.hpp
+++ node.hpp
@@ -7,10 +7,55 @@ protected:
 	int x;
 	int y;
 	/* puntatori alle direzioni */
-	
+	node *nord;
+	node *sud;
+	node *west;
+	node *east;
+	/* true = nodo scavato */
+	bool digged;
 
 public:
+	node(int lat, int lon) {
+		x = lat;
+		y = lon;
+		nord = NULL;
+		sud = NULL;
+		west = NULL;
+		east = NULL;
+		digged = false;
+	}
 
+	int read_x() {
+		return x;
+	}
+
+	int read_y() {
+		return y;
+	}
+
+	node n(){
+		return nord;
+	} 
+
+	node s(){
+		return sud;
+	} 
+
+	node e(){
+		return east;
+	} 
+
+	node w(){
+		return west;
+	} 
+
+	void dig {
+		digged = true;
+	}
+
+	void reactivate {
+		digged = false;
+	}
 
 };
 
