41bb75e8bda96418e0c0ad53384d101c021c4c00
simone preite <simone-preite_89@hotmail.it>
2016-04-17 11:51:33 +0200

    risolto controllo stringa, tronca il nome a MAXNOME

--- header.hpp
+++ header.hpp
@@ -3,6 +3,7 @@
 
 #include <string.h>
 #include <iostream>
+#include <iomanip>
 #include <ctime>
 #include <cstdlib>
 #include "macros.hpp"
--- worms.cpp
+++ worms.cpp
@@ -10,15 +10,14 @@ int main(){
 	ptr_user tmp = NULL;
 	std::cout << "Inserire numero di giocatori (almeno 2 giocatori): \n";
 	std::cin >> num_players;	//inserire il controllo che vengano inseriti soltanto numeri
-	//num_players = num_players;
 	for(i=0; i < num_players; i++){
 		std::cout << "nome_giocatore " << i+1 << "(max 8 caratteri): \n";
-		do{
-			std::cin >> name_player; // inserire controllo lunghezza
-		}while(strlen(name_player)>8);
+  		std::cin >> setw(MAXNOME) >> name_player;
 		std::cout << "lunghezza stringa: " << strlen(name_player) << "\n";
 		tail = enqueue_player(tail, name_player, i); // inizializza lista dei giocatori
 		std::cout << tail << "\n";
+		cin.clear();
+		cin.ignore(1000,'\n');
 	}
 
 	foreach(tail, scan, tmp){
@@ -26,14 +25,10 @@ int main(){
 		scan->user.print_name();
 		std::cout << "\n";
 	}
-	//print_list(tail);
-
-	//estrazione del primo giocatore e posizionamento della tail su quel giocatore
+	//estrazione del primo giocatore
 	primo = random(num_players - 1);
 	std::cout << "numero estratto: " << primo << "\n";
-	/*while(tail->user.print_id() != primo) {
-		tail = tail->next;
-	}*/
+	//posizionamento della tail su quel giocatore con scan
 	foreach(tail, scan, tmp){
 		if(scan->user.print_id() == primo) break;
 	}
