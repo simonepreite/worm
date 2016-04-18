e5493fe3de44401de0d6bc9e347f98e95182240b
simone preite <simone-preite_89@hotmail.it>
2016-04-18 12:02:10 +0200

    up switch-case con collegamento di tutti i nodi

--- auxiliary.cpp
+++ auxiliary.cpp
@@ -20,43 +20,111 @@ ptr_user enqueue_player(ptr_user tail, char name[], int id, ptr_maps p){
 */
 ptr_maps new_node(ptr_maps tail, ptr_maps cur_pos, char direzione){
 	int x, y;
-	ptr_maps tmp;
+	ptr_maps tmp, scan, tmp2;
 	switch (direzione){
 		case 's' : 
 			x = cur_pos->place.read_x();
 			y = cur_pos->place.read_y();
 			tmp = new maps();
-			tmp->place.set(x-1, y);
+			tmp->place.set(x, y-1);
 			tmp->place.n(&cur_pos->place);
 			cur_pos->place.s(&tmp->place);
  			tail = enqueue_map(tail, tmp);
+ 			foreach(tail, scan, tmp2){
+ 				if(scan->place.read_x() == tmp->place.read_x() && scan->place.read_y() == tmp->place.read_y()-1){
+ 					//c'è un nodo a sud
+ 					tmp->place.s(&scan->place);
+ 					scan->place.n(&tmp->place);
+ 				}
+ 				else if(scan->place.read_x() == tmp->place.read_x()+1 && scan->place.read_y() == tmp->place.read_y()){
+ 					//c'è un nodo a est
+ 					tmp->place.e(&scan->place);
+ 					scan->place.w(&tmp->place);
+ 				}
+ 				else if(scan->place.read_x() == tmp->place.read_x()-1 && scan->place.read_y() == tmp->place.read_y()){
+ 					//c'è un nodo a ovest
+ 					tmp->place.w(&scan->place);
+ 					scan->place.e(&tmp->place);
+ 				}
+ 			}
 			break;
 		case 'e' :
 			x = cur_pos->place.read_x();
 			y = cur_pos->place.read_y();
 			tmp = new maps();
-			tmp->place.set(x, y-1);
+			tmp->place.set(x-1, y);
 			tmp->place.w(&cur_pos->place);
 			cur_pos->place.e(&tmp->place);
 			tail = enqueue_map(tail, tmp);
+			foreach(tail, scan, tmp2){
+ 				if(scan->place.read_x() == (tmp->place.read_x()) && scan->place.read_y() == tmp->place.read_y()+1){
+ 					//c'è un nodo a nord
+ 					tmp->place.n(&scan->place);
+ 					scan->place.s(&tmp->place);
+ 				}
+ 				else if(scan->place.read_x() == tmp->place.read_x()+1 && scan->place.read_y() == tmp->place.read_y()){
+ 					//c'è un nodo a est
+ 					tmp->place.e(&scan->place);
+ 					scan->place.w(&tmp->place);
+ 				}
+ 				else if(scan->place.read_x() == tmp->place.read_x() && scan->place.read_y() == tmp->place.read_y()-1){
+ 					//c'è un nodo a sud
+ 					tmp->place.w(&scan->place);
+ 					scan->place.e(&tmp->place);
+ 				}
+ 			}
 			break;
 		case 'w' :
 			x = cur_pos->place.read_x();
 			y = cur_pos->place.read_y();
 			tmp = new maps();
-			tmp->place.set(x, y+1);
+			tmp->place.set(x+1, y);
 			tmp->place.e(&cur_pos->place);
 			cur_pos->place.w(&tmp->place);
 			tail = enqueue_map(tail, tmp);
+			foreach(tail, scan, tmp2){
+ 				if(scan->place.read_x() == (tmp->place.read_x()) && scan->place.read_y() == tmp->place.read_y()+1){
+ 					//c'è un nodo a nord
+ 					tmp->place.n(&scan->place);
+ 					scan->place.s(&tmp->place);
+ 				}
+ 				else if(scan->place.read_x() == tmp->place.read_x()+1 && scan->place.read_y() == tmp->place.read_y()){
+ 					//c'è un nodo a ovest
+ 					tmp->place.w(&scan->place);
+ 					scan->place.e(&tmp->place);
+ 				}
+ 				else if(scan->place.read_x() == tmp->place.read_x() && scan->place.read_y() == tmp->place.read_y()-1){
+ 					//c'è un nodo a sud
+ 					tmp->place.s(&scan->place);
+ 					scan->place.n(&tmp->place);
+ 				}
+ 			}
 			break;
 		default ://va a nord
 			x = cur_pos->place.read_x();
 			y = cur_pos->place.read_y();
 			tmp = new maps();
-			tmp->place.set(x+1, y);
+			tmp->place.set(x, y+1);
 			tmp->place.s(&cur_pos->place);
 			cur_pos->place.n(&tmp->place);
 			tail = enqueue_map(tail, tmp);
+			foreach(tail, scan, tmp2){
+ 				if(scan->place.read_x() == (tmp->place.read_x()) && scan->place.read_y() == tmp->place.read_y()+1){
+ 					//c'è un nodo a nord
+ 					tmp->place.n(&scan->place);
+ 					scan->place.s(&tmp->place);
+ 				}
+ 				else if(scan->place.read_x() == tmp->place.read_x()+1 && scan->place.read_y() == tmp->place.read_y()){
+ 					//c'è un nodo a est
+ 					tmp->place.e(&scan->place);
+ 					scan->place.w(&tmp->place);
+ 				}
+ 				else if(scan->place.read_x() == tmp->place.read_x()-1 && scan->place.read_y() == tmp->place.read_y()){
+ 					//c'è un nodo a ovest
+ 					tmp->place.w(&scan->place);
+ 					scan->place.e(&tmp->place);
+ 				}
+ 			}
 			break;
 	}
 	return tail;
