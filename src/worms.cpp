106a3eb2f88dace5abebe8826440946b7b4442d0
simone preite <simone-preite_89@hotmail.it>
2016-05-06 23:14:55 +0200

    work in prograss nuova stampa

--- src/worms.cpp
+++ src/worms.cpp
@@ -2,6 +2,40 @@
 
 using namespace std;
 
+
+
+
+void stampa_mappa(node* map_tail, player* play){
+	node* scan_map;
+	node* tmp_map;
+	int i, j;
+	int min_lon = play->lon() - 3;
+	int max_lon = play->lon() + 3;
+	int min_lat = play->lat() - 3;
+	int max_lat = play->lat() + 3;
+	int map[7][7];
+
+	
+	for(i=0; i<7; i++){
+		for(j=0; j<7; j++){
+			map[i][j]=0;
+		}
+	}
+
+	foreach(map_tail, scan_map, tmp_map){
+		if(scan_map->read_x() >= min_lon && scan_map->read_x() <= max_lon && scan_map->read_y() >= min_lat && scan_map->read_y() <= max_lat){
+			map[scan_map->read_x()+3][scan_map->read_y()+3] = 1;
+		}
+		else map[scan_map->read_x()+3][scan_map->read_y()+3] = 0;
+	}
+	for(i=0; i<7; i++){
+		for(j=0; j<7; j++){
+			std::cout << " " << map[i][j] << " ";
+		}
+		std::cout << "\n";
+	}
+}
+
 int main(){
 	int i, num_players, primo, n, z;
 	char name_player[MAXNOME];
@@ -72,11 +106,13 @@ int main(){
 	scan->print_name();
 	std::cout << "\n\n";
 	n = 1;
+
 	for(z = 4; z > 0 ; z--){
 		std::cout << z << "\n";
 		if (z == 1) std::cout << "START\n";
 		sleep(1);
 	}
+
 	for(i = MAXTURNI; i > 0; i--){//loop turni
 		char scelta;
 		char dir;
@@ -139,7 +175,7 @@ int main(){
 			std::cout << "\n------------------------------------------------------------------------------------------------------------------\n";
 			std::cout << "centro: " << pam << "\n";
 			//FINE STAMPA MAPPA
-
+			stampa_mappa(map_tail, scan);
 			info_giocatore(scan, i);
 			std::cout << "cur player: " << scan << "\n";
 			std::cout << "i: " << i << "\n";
